99adf59e5a8660fef200df792cea1db7289e9730
gil-son <gilszon@gmail.com>
2021-03-01 11:51:02 -0300

    App

---
+++ app.py
@@ -0,0 +1,35 @@
+# coding: iso-8859-1
+from selenium.webdriver import Chrome
+import time
+from datetime import datetime
+
+url = 'http://fullstackeletro-front.herokuapp.com/'
+
+navegador = Chrome()
+navegador.get(url)
+
+'''
+    - Intervalos de 30 em 30 min
+    - 7h até 0h
+    - Temos 17h → 34 ciclos → +1 = 35
+    - Monitoramento
+    - Horário Limite 
+'''
+
+hora_final = 20
+inicio = 0
+ciclo = 0
+
+while datetime.now().hour < hora_final:
+    if inicio == 0:
+        print("Acordando o site. Aguarde 3 minutos")
+        time.sleep(180)
+        navegador.refresh()
+        inicio = 1
+        print('\t ------ Relatório ------')
+    else:
+        ciclo +=1
+        print('\n Ciclo: {} \n Período: {}'.format(ciclo, datetime.now()))
+        time.sleep(1790)
+        navegador.refresh()
+navegador.quit()
